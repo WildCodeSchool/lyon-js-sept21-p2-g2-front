e37759b7cbcd30758bf74d8b978c78116ae99107
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2021-10-20 17:41:47 +0200

    add tailwind in index.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
-/* eslint disable */
+/* eslint-disable */
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       spacing: {
-        500: "500px",
+        500: '500px',
       },
     },
   },
