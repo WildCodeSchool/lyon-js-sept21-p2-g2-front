30de2a18f925bbd3b1f329dc4dbad2cb4ff36dfb
William <wlouislouisy@gmail.com>
2021-10-21 20:16:55 +0200

    header component made and custom colors added to tailwind config file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Router>
-      <Link to="/">
-        <img src="logo" alt="logo" />
-      </Link>
-      <Link exact to="/login">
-        <button type="submit">Login</button>
-      </Link>
-
-      <Switch>
-        <Route exact path="/">
-          {/* <Home /> */}
-        </Route>
-        <Route path="/login">{/* <Login /> */}</Route>
-      </Switch>
-    </Router>
+    <>
+      <div>
+        <Header />
+      </div>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        'english-red': '#B23A48',
+        'melon-pink': '#FCB9B2',
+        'dark-sienna': '#461220',
+        'apricot-pink': '#FFE4D8',
+      },
       spacing: {
         500: '500px',
       },
