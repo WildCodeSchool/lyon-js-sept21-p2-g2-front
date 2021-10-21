061bfc7a7fc8d9d9ed0f56ad9c01654a7f633460
lea-weibel <lea.weibel00@gmail.com>
2021-10-21 14:57:05 +0200

    create home route linked to logo and login route linked to button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function App() {
-  return <div />;
+  return (
+    <Router>
+      <Link to="/">
+        <img src="logo" alt="logo" />
+      </Link>
+      <Link exact to="/login">
+        <button type="submit">Login</button>
+      </Link>
+
+      <Switch>
+        <Route exact path="/">
+          {/* <Home /> */}
+        </Route>
+        <Route path="/login">{/* <Login /> */}</Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
