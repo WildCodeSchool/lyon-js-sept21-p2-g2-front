0b4f8dd2ecb2759f7b7144fa49d80e939f541927
lea-weibel <lea.weibel00@gmail.com>
2021-10-22 10:56:30 +0200

    implement routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route /*Link*/,
+} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      <Link to="/">
+      {/* <Link to="/">
         <img src="logo" alt="logo" />
       </Link>
       <Link exact to="/login">
         <button type="submit">Login</button>
-      </Link>
+      </Link> */}
 
       <Switch>
         <Route exact path="/">
           {/* <Home /> */}
         </Route>
         <Route path="/login">{/* <Login /> */}</Route>
+
+        <Route path="/contact-us">{/* <Contact /> */}</Route>
+        <Route path="/about-us">{/* <About /> */}</Route>
+        <Route path="/result">{/* <Result /> */}</Route>
+        <Route path="/dashboard">{/* <Dashboard /> */}</Route>
       </Switch>
     </Router>
   );
