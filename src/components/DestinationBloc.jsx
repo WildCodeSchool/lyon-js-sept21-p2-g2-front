4e6a712126159d2130b1752a9d8a40cf1439e02d
William <wlouislouisy@gmail.com>
2021-10-25 17:30:55 +0200

    new logo version's added and DestinationBloc component created

---
+++ src/components/DestinationBloc.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import flag from '../assets/Flag_of_Japan.png';
+import './destinationBloc.css';
+
+export default function DestinationBloc() {
+  return (
+    <div id="destinationBloc" className="bg-dark-sienna text-white">
+      <img id="flag" src={flag} alt="Flag" />
+
+      <span id="destinationName" className="font-bold uppercase">
+        Japan
+      </span>
+
+      <ul id="dataList">
+        <li>capital: Tokyo</li>
+        <li>region: Asia</li>
+        <li>timezones: UTC+09:00</li>
+      </ul>
+
+      <ul id="dataList">
+        <li>
+          curency:{' '}
+          <ul>
+            <li>Japanese yen</li>
+            <li>¥</li>
+          </ul>
+        </li>
+        <li>languages: Japanese</li>
+      </ul>
+    </div>
+  );
+}
