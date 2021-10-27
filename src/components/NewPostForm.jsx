4bdfd8eea5fa3316e43a214528f4c51ef03eec92
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-10-27 12:40:53 +0200

    start to fixed css rules, not all good for now

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -8,27 +8,22 @@ const NewPostForm = () => {
   return (
     <form className="contact-form">
       <h2 id="post-title">Create a new post</h2>
-      <div className="form-content">
-        <h3>User Name :</h3>
-        <input
-          type="text"
-          id="name"
-          name="UserName"
-          // onChange={(e) => setName(e.target.value)}
-          placeholder="User Name"
-          autoComplete="off"
-        />
-        <div id="date-picker">
-          <h3>Date :</h3> <DatePicker />
-        </div>
-        <div id="text-add">
-          <h3>Decription of your post</h3>
-          <textarea name="new-post" id="new-post-area" cols="30" rows="10" />
-        </div>
-        <div id="tagList-import">
-          <TagList />
-        </div>
-      </div>
+
+      <h3>User Name :</h3>
+      <input
+        type="text"
+        id="name"
+        name="UserName"
+        // onChange={(e) => setName(e.target.value)}
+        placeholder="User Name"
+        autoComplete="off"
+      />
+      <h3>Date :</h3>
+      <DatePicker />
+      <h3>Decription of your post</h3>
+      <textarea name="new-post" id="new-post-area" />
+
+      <TagList />
     </form>
   );
 };
