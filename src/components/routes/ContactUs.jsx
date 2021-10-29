a90c41b632c4efc5c681d60e0491f9739b55d176
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-10-29 14:25:39 +0200

    form ok and fonctional, but no css style

--- src/components/routes/ContactUs.jsx
+++ src/components/routes/ContactUs.jsx
@@ -1,9 +1,15 @@
-// import React, { useState } from 'react';
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import emailjs from 'emailjs-com';
+import { useToasts } from 'react-toast-notifications';
+import './ContactUs.css';
 
 const ContactUs = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
   const form = useRef();
+  const { addToast } = useToasts();
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -18,6 +24,13 @@ const ContactUs = () => {
       .then(
         (result) => {
           console.log(result.text);
+          setName('');
+          setMessage('');
+          setPhone('');
+          setEmail('');
+          addToast('Thanks', {
+            appearance: 'success',
+          });
         },
         (error) => {
           console.log(error.text);
@@ -25,82 +38,53 @@ const ContactUs = () => {
       );
   };
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   console.log('ok');
-  //   sendEmail();
-  // };
-
   return (
     <form ref={form} onSubmit={sendEmail}>
       <h2>Contact Us</h2>
-      <h3>Name : </h3>
-      <label htmlFor="name"> </label>
-      <input type="text" id="name" name="user_name" />
-      <h3>Email :</h3>
-      <label htmlFor="email">
-        <input type="email" name="user_email" />
-      </label>
-      <h3>Message :</h3>
-      <label htmlFor="message">
-        <textarea name="message" />
-      </label>
-      <input type="submit" value="Send" />
+      <div className="contact-us-form">
+        <h3>Name : </h3>
+        <label htmlFor="name"> </label>
+        <input
+          type="text"
+          id="name"
+          name="user_name"
+          autoComplete="off"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <h3>Email :</h3>
+        <label htmlFor="email">
+          <input
+            type="email"
+            name="user_email"
+            autoComplete="off"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <h3>Phone (optional) :</h3>
+        <label htmlFor="phone">
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            autoComplete="off"
+            onChange={(e) => setPhone(e.target.value)}
+            value={phone}
+          />
+        </label>
+        <h3>Message :</h3>
+        <label htmlFor="message">
+          <textarea
+            name="message"
+            autoComplete="off"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+        </label>
+        <input type="submit" value="Send" />
+      </div>
     </form>
-    // <form className="contact-form">
-    //
-    //   <div className="form-content">
-    //     <h3>Name : </h3>
-    //     <input
-    //       type="text"
-    //       id="name"
-    //       name="user_name"
-    //       // onChange={(e) => setName(e.target.value)}
-    //       // value={name}
-    //       autoComplete="off"
-    //     />
-    //     <h3>Company : (optional)</h3>
-    //     <input
-    //       type="text"
-    //       id="company"
-    //       name="company"
-    //       // onChange={(e) => setCompany(e.target.value)}
-    //       // value={company}
-    //     />
-    //     <h3>Phone : (optional)</h3>
-    //     <input
-    //       type="text"
-    //       id="phone"
-    //       name="phone"
-    //       // onChange={(e) => setPhone(e.target.value)}
-    //       // value={phone}
-    //     />
-    //     <h3>Email :</h3>
-    //     <div className="email-content">
-    //       <input
-    //         type="mail"
-    //         id="email"
-    //         name="user_email"
-    //         // onChange={(e) => setEmail(e.target.value)}
-    //         // value={email}
-    //         autoComplete="off"
-    //       />
-    //     </div>
-    //     <h3>Your message :</h3>
-    //     <textarea
-    //       id="message"
-    //       name="message"
-    //       // onChange={(e) => setMessage(e.target.value)}
-    //       // value={message}
-    //     />
-    //   </div>
-    //   <input
-    //     type="submit"
-    //     value="Send"
-    //     className="button"
-    //     onClick={sendEmail}
-    //   />
-    // </form>
   );
 };
 
