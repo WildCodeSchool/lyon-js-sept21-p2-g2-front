0a843c971c8c053b6acd2e25630cb6429b8d9319
Jordane <j.laurent1993@gmail.com>
2021-10-29 14:22:39 +0200

    Add Weather API with CSS

--- src/components/routes/Destination.jsx
+++ src/components/routes/Destination.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DestinationBloc from '../DestinationBloc';
+import Weather from '../Weather';
 
 const Destination = () => {
   return (
     <>
       <DestinationBloc />
+      <Weather />
     </>
   );
 };
