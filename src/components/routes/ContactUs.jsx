ca3d80957933b6ec1feaa5cd5bf3b31fb31c4a02
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-10-30 10:15:52 +0200

    all css fixed

--- src/components/routes/ContactUs.jsx
+++ src/components/routes/ContactUs.jsx
@@ -28,52 +28,60 @@ const ContactUs = () => {
           setMessage('');
           setPhone('');
           setEmail('');
-          addToast('Thanks', {
+          addToast('Thanks, your message will be traited quickly', {
             appearance: 'success',
           });
         },
         (error) => {
           console.log(error.text);
+          addToast('Sorry, impossible acces, try again later please!', {
+            appearance: 'error',
+          });
         }
       );
   };
 
   return (
     <form ref={form} onSubmit={sendEmail}>
-      <h2>Contact Us</h2>
       <div className="contact-us-form">
-        <h3>Name : </h3>
-        <label htmlFor="name"> </label>
-        <input
-          type="text"
-          id="name"
-          name="user_name"
-          autoComplete="off"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        <h3>Email :</h3>
-        <label htmlFor="email">
-          <input
-            type="email"
-            name="user_email"
-            autoComplete="off"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
-        </label>
-        <h3>Phone (optional) :</h3>
-        <label htmlFor="phone">
-          <input
-            type="text"
-            id="phone"
-            name="phone"
-            autoComplete="off"
-            onChange={(e) => setPhone(e.target.value)}
-            value={phone}
-          />
-        </label>
-        <h3>Message :</h3>
+        <h2 id="title-contact-us">Contact Us</h2>
+        <div className="grid-form">
+          <h3>Name</h3>
+          <label htmlFor="name">
+            <input
+              type="text"
+              id="name-contact"
+              name="user_name"
+              autoComplete="off"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+          </label>
+          <h3>Email</h3>
+          <label htmlFor="email">
+            <input
+              type="email"
+              name="user_email"
+              autoComplete="off"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </label>
+          <h3>
+            Phone <span id="italic"> (optional) </span>{' '}
+          </h3>
+          <label htmlFor="phone">
+            <input
+              type="text"
+              id="phone"
+              name="phone"
+              autoComplete="off"
+              onChange={(e) => setPhone(e.target.value)}
+              value={phone}
+            />
+          </label>
+        </div>
+        <h3>Message</h3>
         <label htmlFor="message">
           <textarea
             name="message"
@@ -82,7 +90,7 @@ const ContactUs = () => {
             value={message}
           />
         </label>
-        <input type="submit" value="Send" />
+        <input type="submit" id="submit-btn" value="Send" />
       </div>
     </form>
   );
