3066b85b83760f97cae231f06416054626b8ef22
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-11-05 11:32:29 +0100

    resolve conflits befor merge on dev

--- src/components/routes/Destination.jsx
+++ src/components/routes/Destination.jsx
@@ -2,13 +2,9 @@ import React from 'react';
 import DestinationBloc from '../DestinationBloc';
 import Weather from '../Weather';
 import Converter from '../Converter';
-
-import AbstractBlogPost from '../AbstractBlogPost';
-<<<<<<< HEAD
 import MySelectTag from '../MySelectTag';
-=======
+import AbstractBlogPost from '../AbstractBlogPost';
 import CreatePostBtn from '../CreatePostBtn';
->>>>>>> dev
 
 const Destination = () => {
   return (
