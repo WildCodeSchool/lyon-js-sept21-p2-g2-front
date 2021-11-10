72e2e1f2f44867ccfe729d487587080d8e4c429a
William <wlouislouisy@gmail.com>
2021-11-10 14:11:10 +0100

    CSS improved and pictures uploading system added

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -1,35 +1,52 @@
 import React from 'react';
+import { Widget } from '@uploadcare/react-widget';
 import DatePicker from './DatePicker';
 import TagList from './TagList';
 
 import '../css/NewPostForm.css';
 
 const NewPostForm = () => {
+  // const [userName, setUserName] = React.useState();
+  // const [Date, setDate] = React.useState();
+  // const [postText, setPostText] = React.useState();
+  // const [picture, setPicture] = React.useState();
+
   return (
-    <form className="contact-form">
-      <h2 id="post-title">Create a new post</h2>
-      <h3>User Name :</h3>
-      <input
-        type="text"
-        id="name"
-        name="UserName"
-        // onChange={(e) => setName(e.target.value)}
-        placeholder="User Name"
-        autoComplete="off"
-      />
-      <div id="dateArea">
-        <h3>Date :</h3>
-        <DatePicker />
-      </div>
-      <h3>Decription of your post</h3>
-      <textarea name="new-post" id="new-post-area" />
-      <TagList />
-      <h3>Photos :</h3>
-      <div id="downloadPict"> Insérer le modulde de telechargement Photo </div>
-      <button type="submit" id="add-post-btn">
-        POST
-      </button>
-    </form>
+    <div id="formContainer">
+      <form className="contact-form">
+        <h2 id="post-title">Create a new post</h2>
+        <h3>User Name :</h3>
+        <input
+          type="text"
+          id="name"
+          name="UserName"
+          // onChange={(e) => setName(e.target.value)}
+          placeholder="User Name"
+          autoComplete="off"
+        />
+        <div id="dateArea">
+          <h3>Date :</h3>
+          <DatePicker />
+        </div>
+        <h3>Decription of your post</h3>
+        <textarea name="new-post" id="new-post-area" />
+        <TagList />
+        <h3>Pictures :</h3>
+
+        <div id="picturesUpload">
+          <p>
+            <label htmlFor="file">Your file:</label>{' '}
+            <Widget
+              publicKey="383c2db2fc40ae1ac595"
+              id="file"
+              onChange={(info) => console.log('Upload completed:', info)}
+              multiple
+              crop
+            />
+          </p>
+        </div>
+      </form>
+    </div>
   );
 };
 
