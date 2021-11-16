593a4fa514946634a20d4f26f954cda79aa01293
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-11-16 23:22:06 +0100

    work in progress for message to the users

--- src/components/routes/Dashboard.jsx
+++ src/components/routes/Dashboard.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import '../../css/Dashboard.css';
 
 const Dashboard = () => {
   return (
-    <div className="dashbord-area">
-      <h1>Dasboard</h1>
-      <h2>Soon for a better experience</h2>
-    </div>
+    <>
+      <div className="dashboard-container">
+        <h2 className="text-3xl font-extrabold text-dark-sienna tracking-tight sm:text-4xl">
+          Dashboard in progress
+        </h2>
+        <h4 className="text-xl text-english-red">
+          Soon, for a better experience, your private space...
+        </h4>
+      </div>
+      <div className="teasing-container">
+        <h2 className="text-3xl font-extrabold text-dark-sienna tracking-tight sm:text-4xl">
+          Dashboard in progress
+        </h2>
+      </div>
+    </>
   );
 };
 
