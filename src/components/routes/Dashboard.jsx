fd9e713bdd3d187035f26c785568d091ebf1bb89
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-11-16 22:43:50 +0100

    creation to dashboard link in the header to create dashbord area in work

--- src/components/routes/Dashboard.jsx
+++ src/components/routes/Dashboard.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="dashbord-area">
+      <h1>Dasboard</h1>
+      <h2>Soon for a better experience</h2>
+    </div>
+  );
 };
 
 export default Dashboard;
