853f3b460e1bdd04310bd65806383cd31c6e9793
NycoChavanoud <nicolas.chavanoud@gmail.com>
2021-11-17 14:32:28 +0100

    dashboard page workinprogress ok

--- src/components/routes/Dashboard.jsx
+++ src/components/routes/Dashboard.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../css/Dashboard.css';
 
 const Dashboard = () => {
   return (
     <>
+      <h1 id="title-dash">Coming Soon</h1>
       <div className="dashboard-container">
-        <h2 className="text-3xl font-extrabold text-dark-sienna tracking-tight sm:text-4xl">
-          Dashboard in progress
-        </h2>
-        <h4 className="text-xl text-english-red">
-          Soon, for a better experience, your private space...
+        <h4 id="baseline-crew">
+          <Link to="/about-us">by trip@wilders crew </Link>
         </h4>
       </div>
       <div className="teasing-container">
-        <h2 className="text-3xl font-extrabold text-dark-sienna tracking-tight sm:text-4xl">
-          Dashboard in progress
-        </h2>
+        <p id="info-for-users">
+          We prepare a personalized space for you to optimize the sharing of
+          your experiences. For now, do not hesitate to share your travel
+          experiences, and to comment on others.
+        </p>
       </div>
+      <h4 id="return-to-home-title"> Find your destination here</h4>
+      <Link to="/" id="return-home-link">
+        back to search
+      </Link>
     </>
   );
 };
