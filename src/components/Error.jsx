66836bcfabc99d3ba8602bb12423e977388e644c
William <wlouislouisy@gmail.com>
2021-11-17 14:38:09 +0100

    404 error page created

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -9,7 +9,7 @@ export default function Example() {
           </p>
           <div className="sm:ml-6">
             <div className="sm:border-l sm:border-melon-pink sm:pl-6">
-              <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+              <h1 className="text-4xl font-extrabold text-english-red tracking-tight sm:text-5xl">
                 Page not found
               </h1>
               <p className="mt-1 text-base text-english-red">
