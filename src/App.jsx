651377ec0462d88f5bb6cccf8b5fadff1bb4da0e
William <wlouislouisy@gmail.com>
2021-11-18 16:05:20 +0100

    error route restored

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Destination from './components/routes/Destination';
 import PostDetails from './components/routes/PostDetails';
 import Footer from './components/Footer';
 import Quizz from './components/routes/Quizz';
-// import Error from './components/Error';
+import Error from './components/Error';
 import Dashboard from './components/routes/Dashboard';
 import { DisplayPostsProvider } from './contexts/DisplayPostsContext';
 
@@ -29,6 +29,7 @@ function App() {
               <Route path="/posts/:id" component={PostDetails} />
               <Route path="/quizz" component={Quizz} />
               <Route path="/dashboard" component={Dashboard} />
+              <Route path="/error" component={Error} />
             </Switch>
           </main>
           <Footer />
